8ad523d1e661758f2e45cb6e7d06d09bd6830225
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-02 15:12:43 +0530

    Add files via upload

---
+++ begn4.py
@@ -0,0 +1,7 @@
+strn=input("")
+alph=["a","e","i","o","u","b","c","d","f","g","h","j","k","l","m","n","s","p","q","r","t","v","w","x","y","z"]
+if(strn in alph):
+    print("Alphabet")
+else:
+    print("No")
+
