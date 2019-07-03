df6aafa752847b923ae58aab947e6a9fec38e21f
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-03 11:37:06 +0530

    Add files via upload

--- begn15.py
+++ begn15.py
@@ -1,6 +1,14 @@
 n,k=map(int,input("").split())
-n=n+1
-while(n<k):
-    if(n%2==0):
-        print(n,end=" ")
-    n+=1
+i=n+1
+
+while(i<k):
+    f=0
+    for j in range(2,i):
+        if(i%j==0):
+            f=1
+            break
+        j+=1
+    if(f==0):
+        print(i,end=" ")
+    i+=1
+        
