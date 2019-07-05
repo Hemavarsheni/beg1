c2cd433eceb87c3c6968cef4beb9145389005f7d
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-05 11:10:32 +0530

    Update pro7.py

--- pro7.py
+++ pro7.py
@@ -4,7 +4,7 @@ while (2**c<num):
     c=c+1
 if(2**c-1==num):
     print(0)
-elif((num-2)**(c-1)<=2**(c-num)):
+elif(num-(2**(c-1))<=2**(c-num)):
     print(num-(2**(c-1)))
 else:
     print(2**(c-num))
