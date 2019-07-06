cfe3e387f40b23f75e8a12719f74846672955d35
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-06 16:46:33 +0530

    Create hunt22.py

---
+++ hunt22.py
@@ -0,0 +1,12 @@
+num=int(input(""))
+l=list(map(int,input("").split()))
+m=[]
+s=1
+for i in range(0,len(l)):
+    for j in range(0,len(l)):
+        if(i!=j):
+            s=s*l[j]
+    m.append(s)
+    s=1
+for i in m:
+    print(i,end=" ")
