c9917680fe820ca3816ace7c1916a2ad7079b048
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-06 16:59:54 +0530

    Create hunt24.py

---
+++ hunt24.py
@@ -0,0 +1,15 @@
+n,k=map(int,input("").split())
+l=list(map(int,input("").split()))
+f=0
+for i in range(0,len(l)):
+    for j in range(0,len(l)):
+        if(i!=j):
+            if(i+j==k):
+                f=1
+            break
+    if(f==1):
+        break
+if(f==1):
+    print("YES")
+else:
+    print("NO")
