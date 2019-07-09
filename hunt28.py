9e35100dd90459ad78dba5a4a558deb95106d3d6
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-09 07:25:12 +0530

    Update hunt28.py

--- hunt28.py
+++ hunt28.py
@@ -1,8 +1,8 @@
- s=str(input(""))
+si=str(input(""))
 l=[]
-for i in range(0,len(s)):
-    if(s[i] not in l):
-        l.append(s[i])
+for i in range(0,len(si)):
+    if(si[i] not in l):
+        l.append(si[i])
 for i in l:
     print(i,end="")
     
