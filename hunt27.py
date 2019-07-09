f4900a4af27420c41dbf7da090702d59480972f5
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-09 07:57:03 +0530

    Update hunt27.py

--- hunt27.py
+++ hunt27.py
@@ -1,16 +1,11 @@
 s=str(input(""))
 l=[]
 t=""
-for i in range(0,len(s)):
-    for j in range(i,len(s)):
-        t=t+s[j]
-        if(t[::-1]!=t):
-            l.append(t)
-    t=""
-maxv=0
-r=0
-for i in range(0,len(l)):
-    if(len(l[i])>maxv):
-        maxv=len(l[i])
-        r=i
-print(l[r])
+ln=len(s)
+while(ln!=0):
+    t=s[0:ln]
+    if(t[::-1]!=t):
+        print(t)
+        break
+    else:
+        ln=ln-1
