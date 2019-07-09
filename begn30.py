f8d5f7264fcb4d2aff6c11eaf2c09165b64d0e77
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-09 06:48:51 +0530

    Update begn30.py

--- begn30.py
+++ begn30.py
@@ -1,7 +1,7 @@
-h1,m1=map(int,input("").split())
-h2,m2=map(int,input("").split())
-h=h1-h2
-m=m1-m2
+h_1,m_1=map(int,input("").split())
+h_2,m_2=map(int,input("").split())
+h=h-1-h_2
+m=m_1-m_2
 h=abs(h)
 m=abs(m)
 print(h,end=" ")
