1ee14d9563e869d7a5a57c10a6701f641ba90131
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-12 19:22:23 +0530

    Create pro27.py

---
+++ pro27.py
@@ -0,0 +1,20 @@
+n,m=map(int,input().split())
+w=list(map(int,input().split()))
+v=list(map(int,input().split()))
+s=0
+r=0
+t=0
+maxv=0
+for i in range(0,len(w)):
+    for j in range(i,len(w)):
+        if(s+w[j]<=m):
+            s=s+w[j]
+            r=r+v[j]
+        else:
+            break
+    if(s>maxv):
+        maxv=s
+        t=r
+    s=0
+    r=0
+print(t)
