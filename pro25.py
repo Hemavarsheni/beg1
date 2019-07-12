4d0370d5409970d1772463f51dbdaa89328ffaad
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-12 18:20:01 +0530

    Create pro25.py

---
+++ pro25.py
@@ -0,0 +1,16 @@
+n=int(input())
+l=list(map(int,input().split()))
+c=1
+maxv=0
+for i in range(0,len(l)):
+    t=l[i]
+    for j in range(i+1,len(l)):
+        if(t<l[j]):
+            c=c+1
+            t=l[j]
+        else:
+            break
+    if(c>maxv):
+        maxv=c
+    c=1
+print(maxv)
