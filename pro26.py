b6f6d6b14b8c5a0c433bd014168434d93d781cb1
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-12 18:36:50 +0530

    Update pro26.py

--- pro26.py
+++ pro26.py
@@ -1,18 +1,11 @@
 n=int(input())
 l=list(map(int,input().split()))
-c=1
-maxv=0
+t=l[0]
+m=[]
+m.append(t)
 for i in range(0,len(l)):
-    t=l[i]
-    for j in range(i+1,len(l)):
-        if(t<=l[j]):
-            c=c+1
-            t=l[j]
-        elif(t==l[j]):
-            t=l[j]
-        else:
-            break
-    if(c>maxv):
-        maxv=c
-    c=1
-print(maxv)
+    if(t<l[i]):
+        m.append(l[i])
+        t=l[i]
+print(len(m))
+
