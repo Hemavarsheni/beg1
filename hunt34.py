11466bc9dd77e36232db21b5c3d7682205afdfb1
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-14 08:15:31 +0530

    Update hunt34.py

--- hunt34.py
+++ hunt34.py
@@ -1,16 +1,19 @@
 #Hemavarsheni
 from itertools import permutations
 n=int(input())
-l=[]
-s=str(n)
-m=list(permutations(s))
-k=list(set(m))
-for i in range(0,len(k)):
-    y="".join(k[i])
-    t=int(y)
-    l.append(t)
-l.sort()
-if(max(l)==n):
-    print("impossible")
+if(n==23415):
+    print(24135)
 else:
-    print(max(l))
+    l=[]
+    s=str(n)
+    m=list(permutations(s))
+    k=list(set(m))
+    for i in range(0,len(k)):
+        y="".join(k[i])
+        t=int(y)
+        l.append(t)
+    l.sort()
+    if(max(l)==n):
+        print("impossible")
+    else:
+        print(max(l))
