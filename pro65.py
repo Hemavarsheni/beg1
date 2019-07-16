4a4afae226b0efdd6d992ea83deee10bbf3340f2
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-16 20:50:45 +0530

    Create pro65.py

---
+++ pro65.py
@@ -0,0 +1,15 @@
+#Hemavarsheni
+import decimal
+n,k=map(int,input().split())
+l=list(map(int,input().split()))
+b=int(input())
+c=0
+for i in range(0,len(l)):
+    if(i!=k):
+        print(decimal.Decimal(l[i])/decimal.Decimal(2))
+        c=c+(decimal.Decimal(l[i])/decimal.Decimal(2))
+d=decimal.Decimal(b)-decimal.Decimal(c)
+if(d==0.0):
+    print("Bon Appetit")
+else:
+    print(d)
