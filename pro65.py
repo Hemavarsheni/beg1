19a5b288624f7432e9bf44c48d4b04c613eb13e9
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-16 20:53:50 +0530

    Update pro65.py

--- pro65.py
+++ pro65.py
@@ -6,7 +6,6 @@ b=int(input())
 c=0
 for i in range(0,len(l)):
     if(i!=k):
-        print(decimal.Decimal(l[i])/decimal.Decimal(2))
         c=c+(decimal.Decimal(l[i])/decimal.Decimal(2))
 d=decimal.Decimal(b)-decimal.Decimal(c)
 if(d==0.0):
