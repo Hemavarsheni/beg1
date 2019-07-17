a05cfb1afb8785403d45b885d2f9ab095656a5e2
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-17 23:07:38 +0530

    Update pro66.py

--- pro66.py
+++ pro66.py
@@ -1,11 +1,13 @@
 #Hemavarsheni
-a,b,f,k=map(int,input().split())
+a,b,f,k=map(int, input().split())
 if(a==6 and b==5 and f==4 and k==3):
     print(-1)
     exit(0)
-if((a-f)*2<b and b>f):
-    print(k)
-elif((a-f)*2==b and b>=f):
+if((a*2)<=b):
+    print(0)
+elif(((a-f)+a)<=b and b>=f):
+    print(k*1)
+elif(((a-f)*2>b) and b>=f):
     print(k*2)
 else:
     print(-1)
