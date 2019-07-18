e083a55b79209f75decd7c73e00dd947c1a3d615
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-18 23:11:16 +0530

    Update pro4.py

--- pro4.py
+++ pro4.py
@@ -5,11 +5,11 @@ if(len(s1)>=len(s2)):
     for i in range(0,len(s2)):
         c=c+abs(ord(s1[i])-ord(s2[i]))
     for i in s1[len(s2):len(s1)]:
-        c=c+ord(i)-95
+        c=c+ord(i)-96
     print(c)
 else:
     for i in range(0,len(s1)):
         c=c+abs(ord(s2[i])-ord(s1[i]))
     for i in s2[len(s1):len(s2)]:
-        c=c+ord(i)-95
+        c=c+ord(i)-96
     print(c)
