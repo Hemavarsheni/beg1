771fcdc50535848435e011135812047116bfc953
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-07-18 19:15:27 +0530

    Update pro68.py

--- pro68.py
+++ pro68.py
@@ -1,8 +1,6 @@
 #Hemavarsheni
 a,b=map(int,input().split())
-if((a-b)!=0):
-    print(1,end=" ")
-    print(a-b)
+if b==1:
+    print(1,2)
 else:
-    print(0,end=" ")
-    print(0)
+    print(1,a-b)
