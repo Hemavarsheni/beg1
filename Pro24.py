16e36b5e7db47f048a9aff263a963084b6a5bf3f
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-08-04 07:40:08 +0530

    Update Pro24.py

--- Pro24.py
+++ Pro24.py
@@ -3,10 +3,21 @@ import itertools
 n=int(input())
 t=""
 k=tuple()
+temp=[]
+result=[]
+count=0
 l=list(itertools.product([0,1],repeat=n))
-for i in range(0,len(l)):
-    k=l[i]
-    t=""
-    for j in range(0,len(k)):
-        t=t+str(k[j])
-    print("".join(t))
+p=0
+while(p<=n):
+    for i in range(0,len(l)):
+        count=0
+        t=""
+        for j in range(0,len(l[i])):
+            t=t+str(l[i][j])
+            if(l[i][j]==1):
+                count=count+1 
+        if(count==p):
+                result.append(t)
+    p=p+1 
+for i in result:
+    print(i)
