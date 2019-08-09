6d2c8dab763f4bac58378751725b70ef03f5ef5b
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-08-09 21:05:16 +0530

    Update pro5.py

--- pro5.py
+++ pro5.py
@@ -1,6 +1,11 @@
 #Hemavarsheni
-import itertools
+from itertools import product
 n,a,b=map(int,input().split())
+if(n%2!=0):
+    print("NO")
+    exit(0)
+else:
+    x=n//2
 c=0
 if(a==b):
     for i in range(n):
@@ -12,31 +17,25 @@ if(a==b):
             print("NO")
             exit(0)
 l=[]
-m=[]
 r=[]
 l.append(a)
 l.append(b)
-p=n//max(l)
-p=p+1
-for i in range(1,p):
-    m=list(itertools.product(l,repeat=i))
-    for j in range(0,len(m)):
-        A=0
-        B=0
-        for k in range(0,len(m[j])):
-            if(m[j][k]==a):
-                A=A+1
-            else:
-                B=B+1
-        if(A%2==0 and B%2==0):
-            r.append(m[j])
-    m.clear()
+f=0
+for i in range(1,a+b):
+    r=list(product(l,repeat=i))
     for j in range(0,len(r)):
         s=0
         for k in range(0,len(r[j])):
             s=s+r[j][k]
-        if(s==n):
-            print("YES")
-            exit(0)
+        if(s==x and a in r[j] and b in r[j]):
+            f=1 
+            break
+        if(f==1):
+            break
     r.clear()
-print("NO")
+    if(f==1):
+        break
+if(f==1):
+    print("YES")
+else:
+    print("NO")
