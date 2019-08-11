8405b9d287a18f0fb6fea7a94860af1b570e9231
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-08-11 16:40:54 +0530

    Update Pro18.py

--- Pro18.py
+++ Pro18.py
@@ -9,24 +9,28 @@ t=[]
 c=0
 for i in range(0,len(l)):
     t=[]
-    for j in range(0,len(l[i])):
+    t.append(-1)
+    for j in range(0,len(l[i])-1):
         if(l[i][j]==1):
+            c=1
             for k in range(j+1,len(l[i])):
+                f=0
                 if(l[i][k]==1):
-                    f=1
-                    c=0
-                    for x in range(j,k+1):
-                        for y in range(j,k+1):
-                            if(l[x][y]!=1):
+                    c=c+1
+                    for x in range(i,i+c):
+                        for y in range(j,j+c):
+                            if(l[x][y]==1):
+                                f=1
+                            else:
                                 f=0
                                 break
-                        c=c+1
                         if(f==0):
                             break 
                     if(f==1):
                         t.append(c)
                 else:
                     break 
+    #print(t)
     length.append(max(t))   
 res=max(length)
 for i in range(res):
