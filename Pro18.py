37aef06a68c5b02d4b9fae1c3e577ce033c58c17
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-08-11 20:34:11 +0530

    Update Pro18.py

--- Pro18.py
+++ Pro18.py
@@ -1,39 +1,38 @@
 #Hemavarsheni
-n,m=map(int,input().split())
+r,c=map(int,input().split())
 l=[]
-for i in range(n):
+for i in range(r):
     m=list(map(int,input().split()))
     l.append(m)
-length=[]
-t=[]
-c=0
-for i in range(0,len(l)):
-    t=[]
-    t.append(-1)
-    for j in range(0,len(l[i])-1):
+result=[]
+for i in range(len(l)):
+    t=[] 
+    for j in range(len(l[i])):
         if(l[i][j]==1):
-            c=1
-            for k in range(j+1,len(l[i])):
-                f=0
-                if(l[i][k]==1):
+            c=0
+            f=1
+            for k in range(j,len(l[i])):
+                if(l[i][k]==1 and f==1):
                     c=c+1
-                    for x in range(i,i+c):
-                        for y in range(j,j+c):
-                            if(l[x][y]==1):
-                                f=1
-                            else:
-                                f=0
-                                break
+                    for x in range(i,len(l)):
+                        for y in range(j,len(l[i])):
+                            if(x<i+c and y<j+c):
+                                if(l[x][y]!=1):
+                                    f=0
+                            if(f==0):
+                                break 
                         if(f==0):
-                            break 
+                            break
                     if(f==1):
-                        t.append(c)
+                        t.append(c) 
                 else:
-                    break 
-    #print(t)
-    length.append(max(t))   
-res=max(length)
+                    break
+    t.append(0)
+    result.append(max(t))
+res=max(result)
 for i in range(res):
     for j in range(res):
-        print(1,end=" ")
-    print("")
+        if(j==res-1):
+            print(1)
+        else:
+            print(1,end=" ")
