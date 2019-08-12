e26ca41ccc95a7d1340c231c62cdc4820e4faf48
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-08-12 20:39:06 +0530

    Update pro54.py

--- pro54.py
+++ pro54.py
@@ -10,6 +10,9 @@ for i in range(n):
     res.append(l2[i]//l1[i]) 
 minv=min(res) 
 def minimum(n,minv,res,l1,l2,k):
+    #print(res)
+    #print(k)
+    #print(minv)
     for i in range(n):
         if(res[i]==minv):
             t=k 
@@ -18,10 +21,20 @@ def minimum(n,minv,res,l1,l2,k):
                 s=s+1 
                 t=t-1 
                 if((s//l1[i])>minv):
-                    res[i]=res[i]+1
-                    k=t
+                    res[i]=res[i]+1 
+                    l2[i]=s
+                    #print(res[i],end=" ") 
+                    k=t 
+                    #print(k)
                     break 
+    #print("")
+    #print(res,end=" ")
+    #print(k)
     if(minv!=min(res) and k>0):
-        minimum(n,min(res),res,l1,l2,k) 
-minimum(n,minv,res,l1,l2,k)
-print(min(res))
+        return minimum(n,min(res),res,l1,l2,k) 
+    else:
+        return res 
+t=[]
+t=minimum(n,minv,res,l1,l2,k)
+#print("")
+print(min(t))
