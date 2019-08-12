7ccd88670bec10e6d45397e36831054cd5b33d3e
Hemavarsheni <52447543+Hemavarsheni@users.noreply.github.com>
2019-08-12 12:48:56 +0530

    Create pro21.py

---
+++ pro21.py
@@ -0,0 +1,17 @@
+#Hemavarsheni 
+n=int(input())
+l=list(map(int,input().split()))
+c=0
+for i in range(0,len(l)):
+    if(l[i]!="*"):
+        for j in range(0,len(l)):
+            if(l[i]==l[j] and i!=j):
+                c=c+1
+                print(c)
+                l[i]="*"
+                l[j]="*" 
+                break 
+if(c==0):
+    print("no")
+else:
+    print("yes")
